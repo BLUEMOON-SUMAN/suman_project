a61c4ac19cfcfe2ae398fac4c825fe414b7d32a0
kangchan <dodorami213@gmail.com>
2025-07-22 00:48:28 +0900

    Update settings.py

--- suman_pj_back/suman_pj_back/settings.py
+++ suman_pj_back/suman_pj_back/settings.py
@@ -134,6 +134,7 @@ WSGI_APPLICATION = 'suman_pj_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASE_URL = config('DATABASE_URL')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
@@ -142,6 +143,7 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD', default = 'your_local_db_password'),
         'HOST': config('DB_HOST', default = 'localhost'),
         'PORT': config('DB_PORT', default = '5432'),
+        'default' : dj_database_url.parse(DATABASE_URL)       
     }
 }
 
