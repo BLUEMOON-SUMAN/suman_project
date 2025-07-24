b4fe0b32b9bd5e4400542db15ead4d69cac79c57
kangchan <dodorami213@gmail.com>
2025-07-25 04:17:44 +0900

    Update views.py

--- suman_pj_back/Inquiries/views.py
+++ suman_pj_back/Inquiries/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAdminUser
 
 from .models import Inquiry
@@ -10,9 +8,9 @@ class InquiryViewSet(viewsets.ModelViewSet) :
     queryset = Inquiry.objects.all()
     serializer_class = Inquiryserializer
 
-def get_permission(self) :
+def get_permissions(self) :
     if self.request.method == 'POST' :
         self.permission_classes = [AllowAny]
     else :
         self.permission_classes = [IsAdminUser]
-    return super().get_permission()
+    return super().get_permissions()
