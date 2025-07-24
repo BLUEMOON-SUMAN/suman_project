82850d9b9f4c04815fe139677ffb6eaed4e26728
kangchan <dodorami213@gmail.com>
2025-07-25 03:53:37 +0900

    Update serializers.py

--- suman_pj_back/question/serializers.py
+++ suman_pj_back/question/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
-from .models import FAQ
+from .models import FAQ, Category
 
 class FAQserializer(serializers.ModelSerializer) :
+    category = serializers.StringRelatedField()
+    
     class Meta :
         model = FAQ
         fields = '__all__'
+
