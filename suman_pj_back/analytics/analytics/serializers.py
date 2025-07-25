42e27f04c8b0e8dfcd49dd4a83e9bb7d688385be
Seungwon12345 <lesw04100@naver.com>
2025-07-25 13:35:28 +0900

    Delete suman_pj_back/analytics/analytics directory

--- suman_pj_back/analytics/analytics/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class AnalyticsDataSerializer(serializers.Serializer):
-    total_users = serializers.IntegerField(read_only=True)
-    change_percentage = serializers.FloatField(read_only=True)
--- suman_pj_back/analytics/analytics/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from analytics.views import AnalyticsDataViewSet
-from django.urls import path, include
-
-router = DefaultRouter()
-router.register(r'analytics', AnalyticsDataViewSet, basename = 'analytics')
-
-urlpatterns = router.urls
