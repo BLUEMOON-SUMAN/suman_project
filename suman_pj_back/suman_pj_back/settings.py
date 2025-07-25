e2c63b6027d11f305262b25833e78f83c4081bc6
Seungwon12345 <lesw04100@naver.com>
2025-07-25 22:04:06 +0900

    0725

--- suman_pj_back/suman_pj_back/settings.py
+++ suman_pj_back/suman_pj_back/settings.py
@@ -149,7 +149,7 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://www.suman.co.kr/",
+    "https://www.suman.co.kr",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG
